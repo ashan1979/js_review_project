c7333f6980dbe0f699cb32f642ba7cf1e5035837
ashan <indrajee79@gmail.com>
2022-11-17 20:12:23 +0530

    more js

--- script.js
+++ script.js
@@ -6,6 +6,34 @@ const reviews = [
         job: "web developer",
         img:
         "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzsh1.jpg",
-
-    }
-]
+        text:
+        "I'm baby meggins twee health goth +1. biciycle tumeric charteuse before they sold out chambrey pop-up"
+    },
+    {
+        id: 2,
+        name: "anna johnson",
+        job: "web designer",
+        img:
+          "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+        text:
+          "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+    },
+    {
+        id: 3,
+    name: "peter jones",
+    job: "intern",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text:
+      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+    },
+    {
+        id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text:
+      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+    },
+];
