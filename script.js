cfbbbb40a969861a3d6346d2bfa49511b6690031
ashan <indrajee79@gmail.com>
2022-11-17 20:07:10 +0530

    initial js

---
+++ script.js
@@ -0,0 +1,11 @@
+//local reviews data
+const reviews = [
+    {
+        id: 1,
+        name: "suasn smith",
+        job: "web developer",
+        img:
+        "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzsh1.jpg",
+
+    }
+]
